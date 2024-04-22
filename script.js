16805960fe76573ad96e2bcc6d116ea58bf4e754
ageeha927 <ageeha927@west-mec.org>
2024-04-21 20:45:40 -0700

    ..

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
+var app = document.getElementById('typewriter2');
 
+var customNodeCreator = function(character) {
+    return document.createTextNode(character);
+}
 
+var typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 75,
+    onCreateTextNode: customNodeCreator,
+});
+
+typewriter
+    .typeString('CODING STUDENT.')
+    .pauseFor(500)
+    .deleteAll()
+    .typeString('UI DESGINER.')
+    .pauseFor(500)
+    .deleteAll()
+    .typeString('WEBSITE DEVELOPER.')
+    .pauseFor(500)
+    .start();
